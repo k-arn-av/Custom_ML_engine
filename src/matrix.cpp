15311748b5529614853d8696bee644742a502294
k-arn-av <arnavkarna877@gmail.com>
2026-08-15 08:00:40 -0500

    allowed for updated operations required in the feed forward and backward processes

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -1,5 +1,6 @@
 #include "matrix.hpp"
 
+    
 Matrix Matrix::transposed() const {
     Matrix result_matrix(total_columns, total_rows);
     for (size_t r = 0; r < total_rows; ++r) {
@@ -21,7 +22,7 @@ Matrix Matrix::hadamard(const Matrix& first_matrix, const Matrix& second_matrix)
     return result_matrix;
 }
 
-Matrix Matrix::hadamard_inplace(const Matrix& other){
+Matrix& Matrix::hadamard_inplace(const Matrix& other){
     if (this->rows()!= other.rows() || this->columns()!=other.columns()){
         throw std::invalid_argument("error");
     }
@@ -32,15 +33,29 @@ Matrix Matrix::hadamard_inplace(const Matrix& other){
 }
 
 Matrix Matrix::operator+ (const Matrix& other){
-    if (this->rows()!=other.rows()|| this->columns()!=other.columns()){
-        throw std::invalid_argument("error");
-    }
-    Matrix result_matrix(this->rows(), this->columns());
+    if (this->rows() == other.rows() && this->columns() == other.columns()){ // Normal Matrix addition for same sized matrices
 
-    for(size_t i=0;i<this->size();++i){ 
-        result_matrix[i]= (*this)[i]+other[i];
+        Matrix result_matrix(this->rows(), this->columns());
+
+        for(size_t i=0;i<this->size();++i){ 
+            result_matrix[i]= (*this)[i]+other[i];
+        }
+        return result_matrix;  
     }
-    return result_matrix;  
+
+    if (this->rows() == other.rows() && other.columns() == 1){ // specifically for bias addition (broadcasting) Z=WX+B where bias is column matrix, WX is this, while B is other
+         
+        Matrix result_matrix(this->rows(), this->columns());
+
+        for (size_t r=0; r< this->rows(); ++r){
+            for (size_t c=0; c< this->columns(); ++c){
+                result_matrix(r,c)= (*this)(r,c)+ other[r];
+            }
+        }
+        return result_matrix;
+    }
+
+    throw std::invalid_argument("error");
 }
 
 Matrix Matrix::operator- (const Matrix& other) {
@@ -55,9 +70,34 @@ Matrix Matrix::operator- (const Matrix& other) {
     return result_matrix;  
 }
 
-Matrix& Matrix::operator*=(double num) { 
+Matrix& Matrix::operator*= (double num) { 
     for(size_t i=0;i<this->size();++i){ 
         (*this)[i] *= num;
     }
     return *this;
+}
+
+// Matrix multiplication using r-k-c loop, because traversing by column every time is
+// more efficient than traversing by rows due to cpu cache locality (chunks of elements processing at once). Traversing by row is 
+// the worst, since cpu has to jump from one random space to another, and doing it at every step causes cache miss
+// k is the common row and column for first and second matrices, and k only increases row-by-row
+
+Matrix Matrix::operator* (const Matrix& other){
+    if (this->columns()!=other.rows()){
+        throw std::invalid_argument("error");
+    }
+    Matrix result_matrix(this->rows(),other.columns());
+
+    for (size_t r=0;r< this->rows(); ++r){
+        for(size_t k =0;k< this->columns(); ++k){
+            double save_val= (*this)(r,k);
+            for(size_t c=0; c< other.columns(); ++c){
+                
+                result_matrix(r,c)+= save_val *other(k,c);//used k, because notice that the col of A and row of B is iterating first, so replace col A and row B with k
+
+            }
+        }   
+    }
+    return result_matrix;
+
 }
