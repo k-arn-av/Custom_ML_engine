e8295e882f69d379cd683d85c961a211a2776b17
k-arn-av <arnavkarna877@gmail.com>
2026-08-27 05:38:42 -0500

    completed python bindings for the program

--- src/pybindings.cpp
+++ src/pybindings.cpp
@@ -5,6 +5,7 @@
 #include "matrix.hpp"
 #include "Network.hpp"
 #include "optimizer.hpp"
+#include "Layer.hpp"
 #include "ReluAL.hpp"
 #include "DenseLayer.hpp"
 #include "SoftmaxCE.hpp"
@@ -15,15 +16,17 @@ PYBIND11_MODULE(Custom_ML, m){ //python will import Custom_ML
     m.doc()= "MLP engine";
 
     //Matrix Class bindings
-
     py::class_<Matrix>(m, "Matrix")
 
     //Constructors first
         .def(py::init<>()) //empty matrix constructor
+    
         .def(py::init<size_t, size_t, double>(), //main matrix constructor for calculation
             py::arg("r"), py::arg("c"), py::arg("initial_value")=0.0)
+
         .def(py::init<size_t, size_t, std::initializer_list<double>>(), //already initialized vector constructor
             py::arg("r"), py::arg("c"), py::arg("user_data"))
+
         .def(py::init<size_t, size_t, double, double>(), //random weight generator constructor 
             py::arg("r"), py::arg("c"), py::arg("minimum"), py::arg("maximum"))
             
@@ -69,6 +72,86 @@ PYBIND11_MODULE(Custom_ML, m){ //python will import Custom_ML
         .def(py::self* py::self) //matmul
         .def(py::self* double()) //scalar matmul
         .def(double()* py::self)  //reverse scalar matmul
-        .def(py::self*= double()) //in place scalar matmul
-    ;
+        .def(py::self*= double()); //in place scalar matmul
+    
+    //Layer abstract base class bindings
+    py::class_<Layer>(m, "Layer") //layer doesnt have virtual or abstraction lock virtual=0
+        
+        .def("feedforward", &Layer::feedforward, py::arg("input"))
+        .def("feedbackward", &Layer::feedbackward, py::arg("output_gradient"))
+
+        .def("getWeight_Bias", &Layer::getWeight_Bias)
+        .def("get_gradients", &Layer::get_gradients);
+        
+
+    //Denselayer bindings (inheritance syntax)
+    py::class_<DenseLayer, Layer>(m, "DenseLayer")
+        
+        .def(py::init<std::string, size_t, size_t>(),
+            py::arg("n"), py::arg("neurons"), py::arg("input_features"))
+        
+        //overload cast to return non const Weights since py cannot distinct between const and non const same functions
+        .def("Weights", py::overload_cast<> (&DenseLayer::Weights))
+        .def("Bias", py::overload_cast<> (&DenseLayer::Bias))
+
+        .def("getName", &DenseLayer::getName)
+
+        .def("weightGradients", &DenseLayer::weightGradients)
+        .def("biasGradients", &DenseLayer::biasGradients);
+        
+    //Relu bindings
+    py::class_<Relu, Layer>(m, "Relu")
+
+        .def(py::init<std::string>(),
+            py::arg("n"))
+        
+        .def("getName", &Relu::getName);
+        
+    
+    //SoftmaxCE loss layer bindings
+    py::class_<SoftmaxCrossentropy>(m, "SoftmaxCE")
+
+        .def("calculate_loss", &SoftmaxCrossentropy::calculate_loss)
+        .def("feedbackward", &SoftmaxCrossentropy::feedbackward);
+    
+
+    //Optimizer abstract base class bindings
+    py::class_<optimizer>(m, "optimizer")
+
+       .def("GradientDescent", &optimizer::GradientDescent);
+    
+
+    //Vanilla SGD bindings
+    py::class_<SGD, optimizer>(m, "SGD")
+        
+        .def(py::init<double>(),
+            py::arg("LR"));
+    
+    
+    //Network bindings
+    py::class_<Network>(m, "Network")
+
+        .def(py::init<>())
+
+        //set optimizer
+        .def("setOptimizer", [](Network& self, std::unique_ptr<optimizer> opt){
+            self.setOptimizer(std::move(opt));
+        }, py::arg("optimizer"))
+
+        //return reference
+        //create a middleman lambda function since template is blueprint only, and python requires cpp compiler to complile exact create_layer during cmake build
+        .def("create_DenseLayer", [](Network& self, const std::string& name, size_t neurons, size_t features)->DenseLayer& {
+            return self.create_layer<DenseLayer>(name, neurons, features);
+        }, py::return_value_policy::reference_internal) // prohibits python to delete the literal layer from Layers vector inside Network, and lets Network handle it
+        
+        .def("create_ReluLayer", [](Network& self, const std::string& name)->Relu& {
+            return self.create_layer<Relu>(name);
+
+        }, py::return_value_policy::reference_internal)
+        
+
+        //train model binding
+        .def("trainModel", &Network::train_model,
+            py::arg("input_matrix"), py::arg("target"));
+
 }
