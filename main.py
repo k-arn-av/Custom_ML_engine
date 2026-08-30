185db959f3918a46710015f652c7daf6a25b19fe
k-arn-av <arnavkarna877@gmail.com>
2026-08-30 03:46:17 -0500

    basic python layer to train model

---
+++ main.py
@@ -0,0 +1,68 @@
+import sys
+import os 
+
+#need these to look for .pyd file and let python sense it
+build_path=os.path.abspath("build") #translates build folder name into standard os path (windows) eg, C: \Users\fnag...\build
+sys.path.append(build_path) #takes that build path and appends it to sys.path (list of strings of folders),
+                            #so that interpreter can detect build folder for the .pyd cpp compiled binary file
+
+import Custom_ML
+
+def main():
+
+    epoch=0
+    target_loss=0.01
+    current_loss=1 #to run the training loop 
+
+    #bunch of random doubles for input, 4 batches, 2 features each
+    Input=Custom_ML.Matrix(4,2,[
+        0.0, 0.0,  # Inputs for 0 OR 0 -> aligns with row 1 of targets
+        0.0, 1.0,  # Inputs for 0 OR 1 -> aligns with row 2 of targets
+        1.0, 0.0,  # Inputs for 1 OR 0 -> aligns with row 3 of targets
+        1.0, 1.0   # Inputs for 1 OR 1 -> aligns with row 4 of targets
+    ])
+
+    #4 rows/batches, 2 columns/classes. 
+    #[1.0, 0.0] means False. [0.0, 1.0] means True.
+    targets = Custom_ML.Matrix(4, 1, [
+        0.0,  # 0 OR 0 = 0
+        1.0,  # 0 OR 1 = 1
+        1.0,  # 1 OR 0 = 1
+        1.0   # 1 OR 1 = 1
+    ])
+    
+    network=Custom_ML.Network() #create network object
+
+    SGD= Custom_ML.SGD(0.01)
+
+    network.setOptimizer(SGD)
+
+    network.setCurrentfeature(2)
+
+    #automatically pushed into network vector
+    network.addDenseLayer("dense1", 4) #creates W(2*4), 4 is neurons
+    network.addReluLayer("relu1")
+    network.addDenseLayer("dense2", 1)
+
+
+    #loop to train model, return loss after each gradient descent
+    while current_loss>target_loss:
+
+        current_loss=network.trainModel(Input,targets)
+
+        #print every 50 epochs
+        if epoch%50==0:
+            print(f"Epoch: {epoch} | Loss: {current_loss:.6f}")
+        epoch+=1
+
+        #added an emergengy break
+        if epoch>10000:
+            print("Hit max epochs. Stopping.")
+            break
+
+    #final result
+    print(f"Training Complete at Epoch {epoch} with Final Loss: {current_loss:.6f}")
+
+if __name__ == "__main__":
+    main()
+
